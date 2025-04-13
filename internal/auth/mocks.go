cb236a571a41b9748c2243211a16cf2706abc7d1
karthikbhandary2 <karthikbhandary2@gmail.com>
2025-04-13 06:33:13 +0000

    fix: Commented out unused vars

--- internal/auth/mocks.go
+++ internal/auth/mocks.go
@@ -1,8 +1,6 @@
 package auth
 
 import (
-	"time"
-
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -10,12 +8,12 @@ type TestAuthenticator struct {}
 
 const secret = "test"
 
-var testClaims = jwt.MapClaims{
-	"aud": "test-aud",
-	"iss": "test-aud",
-	"sub": int64(42),
-	"exp": time.Now().Add(time.Hour).Unix(),
-}
+// var testClaims = jwt.MapClaims{
+// 	"aud": "test-aud",
+// 	"iss": "test-aud",
+// 	"sub": int64(42),
+// 	"exp": time.Now().Add(time.Hour).Unix(),
+// }
 
 func (a *TestAuthenticator) GenerateToken(claims jwt.Claims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
